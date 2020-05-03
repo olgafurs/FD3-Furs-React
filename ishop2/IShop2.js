5ee1dd60896ad0f306e43a5c22c398f56d5a2f93
olgafurs <olga.m.furs@mail.ru>
2020-05-03 11:19:41 +0300

    Error The prop `cbSelected` is marked as required in `Product2`, but its value is `undefined`

--- ishop2/IShop2.js
+++ ishop2/IShop2.js
@@ -9,13 +9,44 @@
   propTypes: {
     name: React.PropTypes.string.isRequired,
     products: React.PropTypes.array.isRequired
+
+    // products: React.PropTypes.arrayOf(
+    //   React.PropTypes.shape({
+    //     code: React.PropTypes.number.isRequired,
+    //     count: React.PropTypes.number.isRequired,
+    //     productName: React.PropTypes.string.isRequired,
+    //     price: React.PropTypes.string.isRequired,
+    //     urlPhoto: React.PropTypes.string.isRequired,
+    //   })
+    // )
+  },
+  
+  getInitialState: function () {
+    return {
+      selectedItemId: 0,
+      // class: "Product2",   
+    };
+  },
+
+  selectProduct: function(sel) {    
+    console.log(sel);
+    this.setState({ selectedItemId: sel })
   }, 
 
   render: function() {    
-
-    var productsArr=this.props.products.map( v =>
-      React.createElement(Product2, {key:v.code, product: v.productName, price:v.price,  count: v.count, img:v.urlPhoto } )
-    );
+    
+    var itemId = this.state.selectedItemId;
+       
+    var productsArr=this.props.products.map( function(v) {      
+     if (itemId == v.code) {      
+      return React.createElement(Product2, {key:v.code, code:v.code, product: v.productName, 
+        price:v.price,  count: v.count, img:v.urlPhoto, class: 'Product2_', cbSelected:this.selectProduct } );
+    }else {
+      return React.createElement(Product2, {key:v.code, code:v.code, product: v.productName, 
+        price:v.price,  count: v.count, img:v.urlPhoto, class: 'Product2', cbSelected:this.selectProduct } );
+    }    
+       
+  });
 
     return React.DOM.div( {className:'IShop'}, 
       React.DOM.div( {className:'nameIShop'}, this.props.name ),
--- ishop2/Product2.js
+++ ishop2/Product2.js
@@ -2,45 +2,64 @@
 
   displayName: 'Product2',
 
-  propTypes: {    
+  propTypes: { 
+    code: React.PropTypes.number.isRequired, 
     product: React.PropTypes.string.isRequired,
     price: React.PropTypes.string.isRequired,
     count: React.PropTypes.number.isRequired,
     img: React.PropTypes.string.isRequired,    
+    class: React.PropTypes.string.isRequired, 
+    cbSelected: React.PropTypes.func.isRequired, 
   }, 
 
-  selectProductBlock: function(EO) {         
-    var selectedBlock = EO.target.parentNode;    
-    console.log(selectedBlock.className);
-    if (selectedBlock.className != 'Product2') return;
 
-  //   document.getElementsByClassName('Product2').map( v => {
-  //     if (v.classList.contains('highlight')){
-  //     v.classList.remove('highlight')
-  //    }
-  //  }
-  // );
-    
-    selectedBlock.classList.add('Highlight'); 
-   
-  },  
+  // getInitialState: function () {
+  //   return {
+  //     selectedItemId: 0,     
+  //   };
+  // },
 
-  deleteProduct: function(EO) {    
-     EO.stopPropagation(); 
-    var delBlock = EO.target.parentNode;//родитель EO.target, удаляемый блок
-    var questionDelete = confirm("Вы хотите удалить товар?");
-    if (questionDelete) {     
-    delBlock.parentNode.removeChild(delBlock);    
-    } 
+  // inputText: function (EO) {    
+  //   this.setState({ filtered: EO.target.value  }, this.processList);   
+  // },
+
+
+  // selectProductBlock: function(EO) {         
+  //   // var selectedBlock = EO.target.parentNode;    
+  //   // console.log(selectedBlock.className);
+  //   // if (selectedBlock.className != 'Product2') return;    
+  //   // selectedBlock.classList.add('Highlight'); 
+
+  //   var selectedBlock = EO.target.getAttribute('itemId');
+  //   this.setState({ selectedItemId: selectedBlock }, this.processList)
+  // },  
+
+  // processList: function(EO) {
+
+
+  // },
+
+  // deleteProduct: function(EO) {    
+  //    EO.stopPropagation(); 
+  //   var delBlock = EO.target.parentNode;//родитель EO.target, удаляемый блок
+  //   var questionDelete = confirm("Вы хотите удалить товар?");
+  //   if (questionDelete) {     
+  //   delBlock.parentNode.removeChild(delBlock);    
+  //   } 
+  // },
+
+  selectBlockProduct: function (EO) {
+    this.props.cbSelected(this.props.code);
   },
 
   render: function() { 
-    return React.DOM.div({className:'Product2', onClick: this.selectProductBlock},
+    // console.log(this.props.class);
+    return React.DOM.div({className: this.props.class, onClick: this.selectBlockProduct},
     React.DOM.img({className:'Photo', src:`${this.props.img}` ,}),
     React.DOM.p({className:'ProductName',  }, this.props.product),
     React.DOM.p({className:'Price',  }, this.props.price),         
     React.DOM.p({className:'Count', }, `В наличии ${this.props.count} букета(ов)` ),
-    React.DOM.button({className:'', onClick: this.deleteProduct }, `Удалить` ),
+    React.DOM.button({className:'', }, `Удалить` ),
    );
   
   },
