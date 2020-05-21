162850d2ae5364b4248a461f98cbd6c1093a2d21
olgafurs <olga.m.furs@mail.ru>
2020-05-21 21:32:53 +0300

    Progect br2jsx

---
+++ br2jsx/components/Br2jsx.js
@@ -0,0 +1,35 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Br2jsx.css';
+
+import Br2jsx2 from './Br2jsx2';
+
+class Br2jsx  extends React.Component {
+
+  static propTypes = {   
+    text: PropTypes.string.isRequired,     
+  };
+
+  render() {
+    let re = /<br\s?\/?>/;
+    let arr =this.props.text.split(re);
+    let arrLeng= arr.length*2-2;
+
+    for(var i = 0; i<arrLeng; i++){
+     arr.splice(++i, 0, <br/>)
+    }
+    
+    let arrDiv = arr.map( (v, index) => {
+      return (        
+        <Br2jsx2 key={index} element={v}/>    
+     )
+  })
+  return <div className = "Br2jsx">{arrDiv}</div>
+
+}
+}
+
+
+export default Br2jsx;
+
---
+++ br2jsx/components/Br2jsx2.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+
+class Br2jsx2  extends React.Component {
+  render() {    
+   return this.props.element 
+  }
+}
+
+
+export default Br2jsx2;
+
